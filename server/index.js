818fd4c9b898ecaaf9925765db82808d15738eb3
Your Name <you@example.com>
2024-03-02 13:03:59 +0530

    Tried changing CORS Test 2- Removed cors from index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const admin = require("firebase-admin");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require("cors");
-app.use(express.json());
 require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
 const isAuthenticated = require("./middleware/auth");
@@ -14,14 +12,6 @@ const projRoutes = require("./routes/projectRoutes");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
-app.use(
-  cors({
-    origin: "https://se-project-frontend.vercel.app",
-    methods: ["GET", "POST", "PUT"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
 
 mongoose.connect(process.env.DATABASE_URL);
 
