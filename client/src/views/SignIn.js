8d680016da5f2fd852461e9acfed75c31eb50265
Your Name <you@example.com>
2024-03-02 15:11:49 +0530

    Changed link in SignIn.js

--- client/src/views/SignIn.js
+++ client/src/views/SignIn.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { auth, provider } from "../components/config";
 import { signInWithPopup } from "firebase/auth";
-import axios from 'axios'; 
-import { useNavigate } from 'react-router-dom';
-import '../views/SignIn.css';
-import blogo from '../assets/bg2.svg'
-import glogo from '../assets/glogo.png'
-
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "../views/SignIn.css";
+import blogo from "../assets/bg2.svg";
+import glogo from "../assets/glogo.png";
 
 function SignIn() {
   const [user, setUser] = useState(null);
@@ -17,7 +15,7 @@ function SignIn() {
     const email = localStorage.getItem("email");
     if (email) {
       setUser(email);
-      navigate('/home');
+      navigate("/home");
     }
   }, []);
 
@@ -27,7 +25,7 @@ function SignIn() {
         const user = result.user;
         const { displayName, email } = user;
         user.getIdToken().then((idToken) => {
-          localStorage.setItem("idToken",idToken);
+          localStorage.setItem("idToken", idToken);
           setUser(email);
           localStorage.setItem("email", email);
           sendUserDataToBackend(displayName, email, idToken);
@@ -40,40 +38,44 @@ function SignIn() {
   };
 
   const sendUserDataToBackend = (name, email, idToken) => {
-    axios.post("https://se-project-backend-fard.onrender.com/auth", {
-      name,
-      email,
-      idToken
-    }, {
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${idToken}`
-      },
-      mode: "cors"
-    })
-    .then((response) => {
-      if (response.status === 200) {
-        navigate('/home'); // Use history.push to redirect
-      } else if (response.status === 201) {
-        navigate('/register'); // Use history.push to redirect
-      }
-      console.log("User data sent to backend successfully");
-    })
-    .catch((error) => {
-      console.error("Error sending user data to backend:", error);
-    });
+    console.log("Sending data to backend");
+    axios
+      .post(
+        "https://se-project-backend-fard.onrender.com/auth",
+        {
+          name,
+          email,
+          idToken,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${idToken}`,
+          },
+          mode: "cors",
+        }
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          navigate("/home"); // Use history.push to redirect
+        } else if (response.status === 201) {
+          navigate("/register"); // Use history.push to redirect
+        }
+        console.log("User data sent to backend successfully");
+      })
+      .catch((error) => {
+        console.error("Error sending user data to backend:", error);
+      });
   };
 
   return (
     <div className="signin-container">
       <div className="signin-content">
-      <img src={blogo} alt="Logo" className="logo"/>
+        <img src={blogo} alt="Logo" className="logo" />
         <button onClick={handleClick} className="google-button">
-          <img src={glogo} alt="Google Icon"/>
+          <img src={glogo} alt="Google Icon" />
           Sign in with Google
         </button>
-       
-
       </div>
     </div>
   );
