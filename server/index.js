c3dc30e0cdaf4857808f4a7fb53b770e519c8687
Your Name <you@example.com>
2024-03-02 13:55:04 +0530

    Added cors again

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ const admin = require("firebase-admin");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const isAuthenticated = require("./middleware/auth");
 const serviceAccount = require("./auth.json");
 const userRoutes = require("./routes/userRoutes");
 const projRoutes = require("./routes/projectRoutes");
+
+app.use(cors());
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
